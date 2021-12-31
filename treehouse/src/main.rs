bbc8a5bebec71efcdf007b49b11c7c208a123851
ALRW <awerner1@googlemail.com>
2021-12-31 15:02:04 +0000

    Updated to accept variable number of guests

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,13 +1,6 @@
 use std::io::stdin;
 
-fn what_is_your_name() -> String {
-    let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line.");
-    your_name.trim().to_lowercase()
-}
-
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -25,19 +18,39 @@ impl Visitor {
         println!("{}", self.greeting)
     }
 }
+
+fn what_is_your_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line.");
+    your_name.trim().to_lowercase()
+}
+
 fn main() {
-    let visitor_list = [
+    let mut visitor_list = vec![
         Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
         Visitor::new("steve", "Hi Steve, Your milk is in the fridge"),
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
-    println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == your_name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("Please leave, you are not on the list")
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend."))
+                }
+            }
+        }
     }
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
