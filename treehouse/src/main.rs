ce7f497772d214eb73c638323377c4eed2ee5e22
ALRW <awerner1@googlemail.com>
2021-12-31 14:53:52 +0000

    added visitor struct and updated visitor list

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -8,19 +8,36 @@ fn what_is_your_name() -> String {
     your_name.trim().to_lowercase()
 }
 
-fn main() {
-    let mut allow_them_in = false;
-    let visitor_list = ["bert", "steve", "fred"];
-    println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    for visitor in &visitor_list {
-        if visitor == &your_name {
-            allow_them_in = true;
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
         }
     }
-    if allow_them_in {
-        println!("Hello, {}", your_name)
-    } else {
-        println!("Sorry you aren't on the allow list.")
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting)
+    }
+}
+fn main() {
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve, Your milk is in the fridge"),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
+    println!("Hello, what's your name?");
+    let your_name = what_is_your_name();
+    let known_visitor = visitor_list
+        .iter()
+        .find(|visitor| visitor.name == your_name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Please leave, you are not on the list")
     }
 }
